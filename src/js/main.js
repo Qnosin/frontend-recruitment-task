2c5d660e887de8ac543d296f6f559db869af761b
qnosin <putaj.jakub@gmail.com>
2022-06-08 09:53:05 +0200

    added localstorage functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,28 @@
 //Dom Elements
 const btn = document.querySelector('button');
 const wrapper = document.querySelector('.wrapper');
-console.log(btn);
+const popup = document.querySelector('.popup');
+const svgElem = document.querySelector('svg');
+const text = document.querySelector('.text');
 let clickCounter = 0;
 btn.addEventListener('click',()=>{
-    //Creating PoPup
-    console.log('hello');       
-    const popUp = document.createElement('section');
-    const h1 = document.createElement('h1');
-    h1.textContent = 'Alert!';
-    popUp.classList.add('popup');
-    popUp.appendChild(h1);
-    wrapper.appendChild(popUp);
+    //Creating text
+    if(localStorage.getItem('clicker')){
+        clickCounter = localStorage.getItem('clicker');
+        clickCounter++;
+        localStorage.setItem('clicker',clickCounter);
+    }else{
+        clickCounter++;
+        localStorage.setItem('clicker',clickCounter);
+    }
+    text.textContent = `You have clicked ${localStorage.getItem('clicker')} times to related button.`;
+    popup.appendChild(text);
+    popup.style.display = `block`;
+
+    svgElem.addEventListener('click',()=>{
+        popup.style.display = `none`;
+    })
+
+
+    
 })
