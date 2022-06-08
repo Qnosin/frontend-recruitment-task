2c7397c8a80d5886be56507e156d8e1f6aaea11c
qnosin <putaj.jakub@gmail.com>
2022-06-08 12:09:59 +0200

    added a clear button with styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,59 @@
 //Dom Elements
-const btn = document.querySelector('button');
+const buttons = document.querySelectorAll('button');
 const wrapper = document.querySelector('.wrapper');
 const popup = document.querySelector('.popup');
 const svgElem = document.querySelector('svg');
 const text = document.querySelector('.text');
 const backgroundEffect = document.querySelector('.bg__popup__black');
 const body = document.querySelector('body');
+const btnClear = document.createElement('button');
 let clickCounter = 0;
-btn.addEventListener('click',()=>{
-    //Creating text
-    if(localStorage.getItem('clicker')){
-        clickCounter = localStorage.getItem('clicker');
-        clickCounter++;
-        localStorage.setItem('clicker',clickCounter);
-    }else{
-        clickCounter++;
-        localStorage.setItem('clicker',clickCounter);
-    }
-    text.textContent = `You have clicked ${localStorage.getItem('clicker')} times to related button.`;
-    popup.appendChild(text);
-    popup.style.display = `block`;
-    backgroundEffect.style.backgroundColor = `#121212`;
-    backgroundEffect.style.opacity = `50%`;
+buttons.forEach((btn)=>{
+    btn.addEventListener('click',()=>{
+        let wrapperId =  btn.parentElement.parentElement.getAttribute('data-id');
 
-    svgElem.addEventListener('click',()=>{
-        popup.style.display = `none`;
-        backgroundEffect.style.backgroundColor = `#E5E5E5`;
-        backgroundEffect.style.opacity = `100%`;
-    })
-    backgroundEffect.addEventListener('click',(e)=>{
-        if(e.target.value !== ''){
+        //LocalStorage Adding Clicker
+        if(localStorage.getItem(`clicker${wrapperId}`)){
+            clickCounter = localStorage.getItem(`clicker${wrapperId}`);
+            clickCounter++;
+            localStorage.setItem(`clicker${wrapperId}`,clickCounter);
+        }else{
+            clickCounter++;
+            localStorage.setItem(`clicker${wrapperId}`,clickCounter);
+        }
+
+        //Text to render on a popup
+        text.textContent = `You have clicked ${localStorage.getItem(`clicker${wrapperId}`)} times to related button.`;
+        popup.appendChild(text);
+
+        //Reset button
+        if(localStorage.getItem(`clicker${wrapperId}`) >= 5){
+            btnClear.textContent = 'Clear click';
+            btnClear.classList.add('btn-clear')
+            popup.appendChild(btnClear);
+        }
+
+        //display popup
+        popup.style.display = `block`;
+
+        //Blur Effect
+        backgroundEffect.style.backgroundColor = `#121212`;
+        backgroundEffect.style.opacity = `50%`;
+
+        //Exit popup listener
+        svgElem.addEventListener('click',()=>{
             popup.style.display = `none`;
             backgroundEffect.style.backgroundColor = `#E5E5E5`;
             backgroundEffect.style.opacity = `100%`;
-        }
+        })
+        backgroundEffect.addEventListener('click',(e)=>{
+            if(e.target.value !== ''){
+                popup.style.display = `none`;
+                backgroundEffect.style.backgroundColor = `#E5E5E5`;
+                backgroundEffect.style.opacity = `100%`;
+            }
+        })
+      
     })
-  
+
 })
